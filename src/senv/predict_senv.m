c61c1f7d185439bf1c8c041190f5a747a3da84c7
Zhihua Su <zhihua.sophia@gmail.com>
2012-05-06 17:00:58 -0500

    Added opts to predict files

--- src/senv/predict_senv.m
+++ src/senv/predict_senv.m
@@ -3,16 +3,29 @@
 
 %% Usage
 % predOutput=predict_senv(stat,Xnew,infType)
+% predOutput=predict_senv(stat,Xnew,infType,opts)
 %
 % Input
 %
-% * stat: A list containing the maximum likelihood estimators and other
+% stat: A list containing the maximum likelihood estimators and other
 % statistics inherted from senv.
-% * Xnew: The value of X with which to estimate or predict Y.  A p by 1
+% 
+% Xnew: The value of X with which to estimate or predict Y.  A p by 1
 % vector.
-% * infType: A string of characters indicting the inference type,
+% 
+% infType: A string of characters indicting the inference type,
 % the choices can be 'estimation' or 'prediction'.
 %
+% opts: A list containing the optional input parameter, to control the
+% iterations in sg_min. If one or several (even all) fields are not
+% defined, the default settings are used.
+% 
+% * opts.maxIter: Maximum number of iterations.  Default value: 300.
+% * opts.ftol: Tolerance parameter for F.  Default value: 1e-10. 
+% * opts.gradtol: Tolerance parameter for dF.  Default value: 1e-7.
+% * opts.verbose: Flag for print out output, logical 0 or 1. Default value:
+% 0.
+% 
 % Output
 %
 % predOutput: A list containing the results of the inference.
@@ -21,7 +34,9 @@
 % Xnew. An r by 1 vector.
 % * predOutput.covMatrix: The covariance matrix of predOutput.value. An r by r
 % matrix.
-% * predOutput.SE: The standard error of elements in predOutput.value.
+% * predOutput.SE: The standard error of elements in predOutput.value. An r
+% by 1 vector. 
+
 %% Description
 % This function evaluates the scaled envelope model at new value Xnew.  It can
 % perform estimation: find the fitted value when X=Xnew, or prediction:
@@ -41,10 +56,12 @@
 % Y(1,:)'
 % predOutput=predict_senv(stat,Xnew,'prediction')
 
-function predOutput=predict_senv(stat,Xnew,infType)
+function predOutput=predict_senv(stat,Xnew,infType,opts)
 
 if (nargin < 3)
     error('Inputs: stat,Xnew and infType should be specified!');
+elseif (nargin==3)
+    opts=[];
 end
 
 if (~strcmp(infType,'estimation'))&&(~strcmp(infType,'prediction'))
