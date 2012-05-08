2b2784044ad317871ac61126a2c679ad11ba089d
Zhihua Su <zhihua.sophia@gmail.com>
2012-05-08 00:53:50 -0500

    1. Changed the interface of bstrp_penv, so that X is now a list 2. Formatted bstrp_penv

--- src/penv/bstrp_penv.m
+++ src/penv/bstrp_penv.m
@@ -2,29 +2,40 @@
 % Compute bootstrap standard error for the partial envelope model. 
 
 %% Syntax
-% bootse=bstrp_penv(X1,X2,Y,u,B)
-% bootse=bstrp_penv(X1,X2,Y,u,B,Opts)
+% bootse = bstrp_penv(X, Y, u, B)
+% bootse = bstrp_penv(X, Y, u, B, Opts)
 %
 % Input
 %
-% * X1: Predictors of main interst. An n by p1 matrix, n is the number of 
+% X: A list containing the value of X1 and X2.
+% 
+% * X.X1: Predictors of main interst. An n by p1 matrix, n is the number of 
 % observations, and p1 is the number of main predictors. The
 % predictors can be univariate or multivariate, discrete or continuous.
-% * X2: Covariates, or predictors not of main interest.  An n by p2 matrix,
-% p2 is the number of covariates.  The covariates can be univariate or 
-% multivariate, discrete or continuous.
-% * Y: Multivariate responses, an n by r matrix, r is the number of
+% * X.X2: Covariates, or predictors not of main interest.  An n by p2 matrix,
+% p2 is the number of covariates.
+% 
+% Y: Multivariate responses, an n by r matrix, r is the number of
 % responses and n is number of observations.  The responses must be continuous variables.
-% * B: Number of boostrap samples.  A positive integer.
-% * u: Dimension of the partial envelope subspace.  A positive integer between 0 and
+% 
+% B: Number of boostrap samples.  A positive integer.
+% 
+% u: Dimension of the partial envelope subspace.  A positive integer between 0 and
 % r.
-% * Opts: A list containing the optional input parameter. If one or several (even all) 
-% fields are not defined, the default settings (see make_opts documentation) 
-% are used.
+% 
+% Opts: A list containing the optional input parameter, to control the
+% iterations in sg_min. If one or several (even all) fields are not
+% defined, the default settings are used.
+% 
+% * Opts.maxIter: Maximum number of iterations.  Default value: 300.
+% * Opts.ftol: Tolerance parameter for F.  Default value: 1e-10. 
+% * Opts.gradtol: Tolerance parameter for dF.  Default value: 1e-7.
+% * Opts.verbose: Flag for print out output, logical 0 or 1. Default value:
+% 0.
 %
 % Output
 %
-% * bootse: The standard error for elements in $$\beta_1$ computed by
+% bootse: The standard error for elements in $$\beta_1$ computed by
 % bootstrap.  An r by p1 matrix.
 
 %% Description
@@ -33,41 +44,42 @@
 
 %% Example
 % load T7-7.dat
-% Y=T7_7(:,1:4);
-% X=T7_7(:,5:7);
-% X1=X(:,3);
-% X2=X(:,1:2);
-% alpha=0.01;
-% u=lrt_penv(X1,X2,Y,alpha)
-% B=100;
-% bootse=bstrp_penv(X1,X2,Y,u,B)
+% Y = T7_7(:, 1 : 4);
+% Xtemp = T7_7(:, 5 : 7);
+% X.X1 = Xtemp(:, 3);
+% X.X2 = Xtemp(:, 1 : 2);
+% alpha = 0.01;
+% u = lrt_penv(X, Y, alpha)
+% B = 100;
+% bootse = bstrp_penv(X, Y, u, B)
 
-function bootse=bstrp_penv(X1,X2,Y,u,B,Opts)
+function bootse = bstrp_penv(X, Y, u, B, Opts)
 
-if (nargin < 4)
-    error('Inputs: X1, X2, Y, u and B should be specified!');
-elseif (nargin==4)
-    Opts=[];
+if nargin < 4
+    error('Inputs: X, Y, u and B should be specified!');
+elseif nargin == 4
+    Opts = [];
 end
 
+X1 = X.X1;
+X2 = X.X2;
+[n r] = size(Y);
+p1 = size(X1, 2);
 
-[n r]=size(Y);
-p1=size(X1,2);
+ModelOutput = penv(X, Y, u, Opts);
 
-ModelOutput=penv(X1,X2,Y,u,Opts);
+Yfit = ones(n, 1) * ModelOutput.alpha' + X1 * ModelOutput.beta1' + X2 * ModelOutput.beta2';
+resi = Y - Yfit;
 
-Yfit=ones(n,1)*ModelOutput.alpha'+X1*ModelOutput.beta1'+X2*ModelOutput.beta2';
-resi=Y-Yfit;
+bootBeta1 = zeros(B, r * p1);
 
-bootBeta1=zeros(B,r*p1);
-
-for i=1:B
+for i = 1 : B
     
-    bootresi=resi(randsample(1:n,n,true),:);
-    Yboot=Yfit+bootresi;
-    temp=penv(X1,X2,Yboot,u,Opts);
-    bootBeta1(i,:)=reshape(temp.beta1,1,r*p1);
+    bootresi = resi(randsample(1 : n, n, true), :);
+    Yboot = Yfit + bootresi;
+    temp = penv(X, Yboot, u, Opts);
+    bootBeta1(i, :) = reshape(temp.beta1, 1, r * p1);
     
 end
 
-bootse=reshape(sqrt(diag(cov(bootBeta1,1))),r,p1);
+bootse = reshape(sqrt(diag(cov(bootBeta1, 1))), r, p1);
