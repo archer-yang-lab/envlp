d8f997108f9a4fa698496f37a86bc87dfd914ee7
Zhihua Su <zhihua.sophia@gmail.com>
2012-05-08 22:43:05 -0500

    Added checking to aic_senv and bic_senv

--- src/senv/bic_senv.m
+++ src/senv/bic_senv.m
@@ -8,18 +8,26 @@
 %
 % Input
 %
-% * X: Predictors. An n by p matrix, p is the number of predictors and n 
+% X: Predictors. An n by p matrix, p is the number of predictors and n 
 % is the number of observations. The predictors can be univariate or 
 % multivariate, discrete or continuous.
-% * Y: Multivariate responses. An n by r matrix, r is the number of
+% 
+% Y: Multivariate responses. An n by r matrix, r is the number of
 % responses. The responses must be continuous variables.
-% * Opts: The optional input parameter. If one or several (even all) 
-% fields are not defined, the default settings (see make_opts documentation) 
-% are used.
+%
+% Opts: A list containing the optional input parameter, to control the
+% iterations in sg_min. If one or several (even all) fields are not
+% defined, the default settings are used.
+% 
+% * Opts.maxIter: Maximum number of iterations.  Default value: 300.
+% * Opts.ftol: Tolerance parameter for F.  Default value: 1e-10. 
+% * Opts.gradtol: Tolerance parameter for dF.  Default value: 1e-7.
+% * Opts.verbose: Flag for print out output, logical 0 or 1. Default value:
+% 0. 
 %
 % Output
 %
-% * u: Dimension of the inner envelope. An integer between 0 and r.
+% u: Dimension of the inner envelope. An integer between 0 and r.
 % 
 %% Description
 % This function implements the Bayesian information criteria (BIC) to select
@@ -33,6 +41,12 @@
 % u=bic_senv(X,Y)
 
 function u=bic_senv(X,Y,Opts)
+
+if nargin < 2
+    error('Inputs: X and Y should be specified!');
+elseif nargin == 2
+    Opts = [];
+end
 
 [n r]=size(Y);
     
