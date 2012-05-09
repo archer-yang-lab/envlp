5f697f6e0159d4c378dfb32f6a570c9eb028bc3a
Yi Yang <emeryyi@gmail.com>
2012-05-09 00:50:03 -0500

    1. doc for F and dF

--- src/sg_min_rev/make_F.m
+++ src/sg_min_rev/make_F.m
@@ -1,20 +1,33 @@
-function F = make_F(fun_method_handle,FParameters)
+%% make_F
+% Generic function to generate the objective function F.
+ 
+%% Syntax
+% F = make_F(fun_method_handle, FParameters)
+% 
+% Input
 %
-% f = F(fun_handle,FParameters)
+% * fun_method_handle: A specific model objective function.
+% * FParameters: A structure that contains data parameters as input for
+% the function fun_method_handle.
 %
-% Generic function to compute the objective function F. The function first 
-% sets a handle to the specific model function and fixes the parameters from 
+% Output
+%
+% * F: The generic objective function for computing the inner envelope subspace.
+%
+
+%% Description
+%
+% Generic function to generate the objective function F. The function first 
+% sets a handle to the specific model function and fixes the data parameters from 
 % the sample needed for its computation. The handle fixed with those 
-% parameters is then evaluated at a given value for argument W.
-%
-%   fun_handle: handle to the objective function for a specific model.
-%   FParameters: fixed parameters needed to evaluate the objective function.
-%   W: argument for computing the derivative.
-% =========================================================================
+% parameters is then evaluated at a given value for argument W. A generic objective
+% function F is returned.
+
+function F = make_F(fun_method_handle, FParameters)
 
 F = @tmp;
-    function fval = tmp(W,varargin)
-        fval = fun_method_handle(W,FParameters,varargin{:});
+    function fval = tmp(W, varargin)
+        fval = fun_method_handle(W, FParameters, varargin{:});
     end
 end
 
