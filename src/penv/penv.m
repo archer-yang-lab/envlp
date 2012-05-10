4bff801371aa5dc8ab4b7c749a75f8292088afa0
Yi Yang <emeryyi@gmail.com>
2012-05-09 19:33:42 -0500

    1. change reference from bullet to number

--- src/penv/penv.m
+++ src/penv/penv.m
@@ -81,9 +81,9 @@
 
 %% References
 % 
-% * The codes is implemented based on the algorithm in Section 3.2 of Su
+% # The codes is implemented based on the algorithm in Section 3.2 of Su
 % and Cook (2012).
-% * The Grassmann manifold optimization step calls the package sg_min 2.4.1
+% # The Grassmann manifold optimization step calls the package sg_min 2.4.1
 % by Ross Lippert (http://web.mit.edu/~ripper/www.sgmin.html).
 
 %% Example
