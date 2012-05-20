8127e95f5f53a2fcbd796f13f1d60decf8fa5221
Zhihua Su <zhihua.sophia@gmail.com>
2012-05-19 22:22:46 -0500

    Added checking the range of alpha in all the lrt files

--- src/penv/lrt_penv.m
+++ src/penv/lrt_penv.m
@@ -59,6 +59,10 @@ elseif nargin == 3
     Opts = [];
 end
 
+if (alpha < 0 || alpha > 1)
+    error('alpha should be between [0, 1]!');
+end
+
 Opts = make_opts(Opts);
 printFlag = Opts.verbose;
 Opts.verbose = 0;
