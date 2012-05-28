dca531ad5a11d85f194ecc8e29376c10d56a6fa5
Zhihua Su <zhihua.sophia@gmail.com>
2012-05-28 02:18:57 -0500

    1. Minor change to documentation of the bootstrap functions. 2. Added wrapper for mfoldcv.

---
+++ src/tools/mfoldcv.m
@@ -0,0 +1,67 @@
+%% mfoldcv
+% Select the dimension for the envelope family using m-fold cross
+% validation.
+
+%% Syntax
+%         u = mfoldcv(X, Y, m, modelType)
+%         u = mfoldcv(X, Y, m, modelType, Opts)
+% 
+%% Input
+%
+% *X*: Predictors.  The predictors can be univariate or multivariate, 
+% discrete or continuous.  
+%
+% *Y*: Multivariate responses. An n by r matrix, r is the number of
+% responses and n is number of observations. The responses must be 
+% continuous variables.
+% 
+% *modelType*: A string characters indicting the model, choices can be
+% 'envpls' or 'xenvpls'.
+% 
+% *Opts*: A list containing the optional input parameters. If one or
+% several (even all) fields are not defined, the default settings are used.
+% 
+% * Opts.verbose: Flag for print out the number of bootstrap samples, 
+% logical 0 or 1. Default value: 0.
+%
+%% Output
+%
+%  *u*: The dimension of the envelope subspace selected by m-fold cross
+%  validation.
+
+%% Description
+% This function implements m-fold cross validation to select the dimension
+% of the envelope space, based on prediction performance.  For each u, the
+% data is partitioned into m parts, each part is in turn used for testing 
+% for the prediction performance while the rest m-1 parts are used for 
+% training.  The dimension is select as the one that minimizes the average 
+% prediction errors. If Y is multivariate, the identity inner product is 
+% used for computing the prediction errors.
+
+%% Example
+%         load wheatprotein.txt
+%         X = wheatprotein(:, 8);
+%         Y = wheatprotein(:, 1 : 6);
+%         m = 5;
+%         modelType = 'envpls';
+%         u = mfoldcv(X, Y, m, modelType)
+
+
+function u = mfoldcv(X, Y, m, modelType, Opts)
+
+% Verify and initialize the parameters
+%
+if nargin < 4
+    error('Inputs: X, Y, m and modelType should be specified!');
+elseif nargin == 4
+    Opts = [];
+end
+
+switch(modelType)
+    case 'envpls'
+        u = mfoldcv_envpls(X, Y, Opts);
+    case 'xenvpls'
+        u = mfoldcv_xenvpls(X, Y, Opts);
+    otherwise
+        fprintf('The value specified in modelType is not supported!');
+end
