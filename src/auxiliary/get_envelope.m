6a29a7b979a89bd026efdd423b9d1b7a4886ac5f
Zhihua Su <zhihua.sophia@gmail.com>
2012-05-28 01:37:35 -0500

    Added documentation for get_envelope.

--- src/auxiliary/get_envelope.m
+++ src/auxiliary/get_envelope.m
@@ -1,6 +1,55 @@
+%% get_envelope
+% Construct the envelope subspace using a sequential algorithm.
+
+%% Syntax
+%         W = get_envelope(S, M, u)
+%
+%% Input
+%
+% *S*: An r by p matrix whose columns span the subspace, the rank of S
+% cannot be greater than u. 
+% 
+% *M*: An r by r positive semi-definite matrix.
+%
+% *u*: Dimension of the envelope. An integer between 0 and r.
+%
+
+%% Output
+%
+% *W*: An r by u semi-orthogonal matrix that spans the M-envelope of
+% span(S).
+
+%% Description
+% This function constructs the M-envelope of span(S) using a sequential
+% algorithm similar to partial least squares.
+
+%% Reference
+% # The codes are implemented based on the algorithm in the lecture notes
+% of Cook (2012).
+
+
 function W = get_envelope(S, M, u)
 
+if nargin < 3
+    error('Inputs: S, M and u should be specified!');
+end
+
 r = size(M, 1);
+r1 = size(S, 1);
+p = rank(S);
+
+if r ~= r1
+    error('The size of S is not valid.');
+end
+
+if p > u
+    error('The rank of S cannot be greater than u.');
+end
+
+if sum(sum(M ~= M')) > 0 || sum(eig(M) > 0) ~= r
+    error('M must be positive definite.');
+end
+
 U = S * S';
 
 [w, D] = eig(U);
