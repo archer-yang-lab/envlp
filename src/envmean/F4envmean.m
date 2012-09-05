78687f1ad41b334f8961b37a46aa8cc0a43164d9
Zhihua Su <zhihua.sophia@gmail.com>
2012-09-04 20:36:38 -0400

    Changed X into Y in F4envmean and dF4envmean

--- src/envmean/F4envmean.m
+++ src/envmean/F4envmean.m
@@ -26,14 +26,14 @@ function f = F4envmean(R, DataParameter)
 
 n = DataParameter.n;
 p = DataParameter.p;
-sX = DataParameter.sX;
-sigX = DataParameter.sigX;
-logDetSX = DataParameter.logDetSX;
+sY = DataParameter.sY;
+sigY = DataParameter.sigY;
+logDetSY = DataParameter.logDetSY;
 
-eigtem = eig(R' * sigX * R);
+eigtem = eig(R' * sigY * R);
 a = log(prod(eigtem(eigtem > 0)));
 
-eigtem0 = eig(R' * inv(sX) * R);
+eigtem0 = eig(R' * inv(sY) * R);
 b = log(prod(eigtem0(eigtem0 > 0)));
 
-f = n * p * (1 + log(2 * pi)) + n * (a + b + logDetSX);
+f = n * p * (1 + log(2 * pi)) + n * (a + b + logDetSY);
