708cb161d578decd85dec89c9f2a98130e929eab
Zhihua Su <zhihua.sophia@gmail.com>
2013-04-14 17:24:09 -0400

    Fixed a bug in sg_min.  In the original version, when r=2, u=1, the iteration will never stop because of the code on line 94 (original line).  On line 94, when r=2, u=1, dim=1, and we always have rem(N,dim)=0.  This problem is fixed by adding an "if statement" to check if r=2, u=1, so that this case is treated differently.

--- src/sg_min_rev/sg_prcg.m
+++ src/sg_min_rev/sg_prcg.m
@@ -36,63 +36,66 @@ function [fn,Yn]= sg_prcg(F,dF,Y)
     end
 
     reset = 1;
-    while ((mag>geps) || (abs(oldf/f-1)>feps) || reset) && (N < maxiter)
-	N= N+1;
+        while ((mag>geps) || (abs(oldf/f-1)>feps) || reset) && (N < maxiter)
+            N= N+1;
 
-	gradsat = (mag<=geps);
-	fsat = (abs(oldf/f-1)<=feps);
-	if (fsat) fun = 'gradline'; else fun = 'Fline'; end
+            gradsat = (mag<=geps);
+            fsat = (abs(oldf/f-1)<=feps);
+            if (fsat) fun = 'gradline'; else fun = 'Fline'; end
 
-	rho = ip(Y,g,g);
-	dr = -g;
-	if (~reset)
-		alpha = -rho/oldrho;
-		dr = dr-alpha*olddr;
-	else reset=0; end
+            rho = ip(Y,g,g);
+            dr = -g;
+            if (~reset)
+                alpha = -rho/oldrho;
+                dr = dr-alpha*olddr;
+            else reset=0; end
 
-	gdr = ip(Y,dr,-g); dr = dr*sign(gdr); gdr=abs(gdr);
-	Hessdr = dgrad(dF,Y,dr); drHdr = ip(Y,dr,Hessdr);
-	cga = abs(gdr/drHdr);
-	if (fsat)
-		% we cannot call fzero unless the function values have different sign
-    while (dFline(0,Y,dr)>0) == (dFline(2*cga,Y,dr)>0)
-	    cga = 2*cga;
-%        disp(strcat('   value of dFline(0,Y,dr) = ',num2str(dFline(0,Y,dr))))
- %      disp(strcat('   value of dFline(2*cga,Y,dr) = ',num2str(dFline(2*cga,Y,dr))))
-    end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		cgb = fzero(dFline,[0,2*cga],[],Y,dr);
-%        disp(strcat('   value of cgb = ',num2str(cgb)))
-	else
-		cgb = fminbnd(Fline,-cga,cga,[],Y,dr);
-%         disp(strcat('   value of cgb = ',num2str(cgb)))    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	end
-	[Ya,dra] = move(Y,dr,cga);
-	[Yb,drb] = move(Y,dr,cgb);
-	if (fsat)
-		ga = grad(dF,Ya);
-		gb = grad(dF,Yb);
-		maga = sqrt(ip(Ya,ga,ga)); magb = sqrt(ip(Yb,gb,gb));
-		if (maga<magb) mag = maga; Y = Ya; g = ga; olddr=dra;
-		else mag = magb; Y = Yb; g = gb; olddr = drb; end
-		newf = F(Y);
-	else
-		fa = F(Ya);
-		fb = F(Yb);
-		if (fa<fb) newf = fa; Y = Ya; olddr = dra;
-		else newf= fb; Y = Yb; olddr = drb; end
-		g = grad(dF,Y); mag = sqrt(ip(Y,g,g));
-	end
- 
-	oldf= f; f = newf; 
-	oldrho = rho;
-	if (SGParameters.verbose)
-		SGdata(N+1,:) = [N mag f];
-		disp(sprintf('%d\t%e\t%e',N,mag,f));
-	end
-	if (rem(N,dim)==0) reset=1; end
-    end
+            gdr = ip(Y,dr,-g); dr = dr*sign(gdr); gdr = abs(gdr);
+            Hessdr = dgrad(dF,Y,dr); drHdr = ip(Y,dr,Hessdr);
+            cga = abs(gdr/drHdr);
+            if (fsat)
+                % we cannot call fzero unless the function values have different sign
+            while (dFline(0,Y,dr)>0) == (dFline(2*cga,Y,dr)>0)
+                cga = 2*cga;
+        %        disp(strcat('   value of dFline(0,Y,dr) = ',num2str(dFline(0,Y,dr))))
+         %      disp(strcat('   value of dFline(2*cga,Y,dr) = ',num2str(dFline(2*cga,Y,dr))))
+            end
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                cgb = fzero(dFline,[0,2*cga],[],Y,dr);
+        %        disp(strcat('   value of cgb = ',num2str(cgb)))
+            else
+                cgb = fminbnd(Fline,-cga,cga,[],Y,dr);
+        %         disp(strcat('   value of cgb = ',num2str(cgb)))    
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            end
+            [Ya,dra] = move(Y,dr,cga);
+            [Yb,drb] = move(Y,dr,cgb);
+            if (fsat)
+                ga = grad(dF,Ya);
+                gb = grad(dF,Yb);
+                maga = sqrt(ip(Ya,ga,ga)); magb = sqrt(ip(Yb,gb,gb));
+                if (maga<magb) mag = maga; Y = Ya; g = ga; olddr = dra;
+                else mag = magb; Y = Yb; g = gb; olddr = drb; end
+                newf = F(Y);
+            else
+                fa = F(Ya);
+                fb = F(Yb);
+                if (fa<fb) newf = fa; Y = Ya; olddr = dra;
+                else newf= fb; Y = Yb; olddr = drb; end
+                g = grad(dF,Y); mag = sqrt(ip(Y,g,g));
+            end
+
+            oldf= f; f = newf; 
+            oldrho = rho;
+            if (SGParameters.verbose)
+                SGdata(N+1,:) = [N mag f];
+                disp(sprintf('%d\t%e\t%e',N,mag,f));
+            end
+            if (dim > 1)
+                if (rem(N,dim)==0) reset=1; end
+            end
+        end
+
     fn = f;
     Yn = Y;
     
